cd81701a9dbbba449a376ae38c0e789ebcf9a934
Glauber Castro <glauber.castro@insa.gov.br>
2022-11-09 13:49:10 -0300

    Atualizaçao do codigo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 app.use(cors())
 
 app.use(
-    express.urlencoded({extended: true}), 
+    express.urlencoded({ extended: true }),
     express.json(),
 )
 
@@ -17,50 +17,50 @@ const port = process.env.PORT
 app.listen(port, () => console.log("funfando"));
 
 // Esta rota filtra uma planta por caracteres de seu nome popular através do queryParams.
-app.get('/search', async (req, res) =>{    
-    try{
+app.get('/search', async (req, res) => {
+    try {
         let name = req.query["nome"]
         const plantas = await Planta.find()
 
-        if(name){
-            var regex = new RegExp( name, 'i' )
-            const planta = await Planta.find({ "nome_popular": { $regex: regex } })
+        if (name) {
+            var regex = new RegExp(name, 'i')
+            const planta = await Planta.find({ "nomePopular": { $regex: regex } })
             res.status(200).json(planta)
-        }else{
+        } else {
             res.status(200).json(plantas)
         }
-    } catch(error){
-        res.status(500).json({error: error})
+    } catch (error) {
+        res.status(500).json({ error: error })
     }
 });
 
-app.get('/', async (req, res) =>{    
-    try{
+app.get('/', async (req, res) => {
+    try {
         const planta = await Planta.find()
 
         res.status(200).json(planta)
 
-    } catch(error){
-        res.status(500).json({error: error})
+    } catch (error) {
+        res.status(500).json({ error: error })
     }
 });
 
-app.get('/planta/:nome_popular', async (req, res) =>{
-    const nome_planta = req.params.nome_popular
+app.get('/planta/:nomePopular', async (req, res) => {
+    const nomePopular = req.params.nomePopular
 
-    try{
-        const plantaTal = await Planta.findOne({ nome_popular: nome_planta })
+    try {
+        const plantaTal = await Planta.findOne({  nomePopular: nomePopular.toLocaleUpperCase()})
 
         res.status(200).json(plantaTal)
-    } catch(error){
-        res.status(500).json({error: error})
+    } catch (error) {
+        res.status(500).json({ error: error })
     }
 });
 
-app.get('/plantas/orderedByName', async (req, res) =>{
-    
+app.get('/plantas/orderedByName', async (req, res) => {
+
     try {
-        const plantas = await Planta.find().sort({ nome_popular: 1 })
+        const plantas = await Planta.find().sort({  nomePopular: 1 })
 
         res.status(200).json(plantas)
     } catch (error) {
@@ -69,25 +69,60 @@ app.get('/plantas/orderedByName', async (req, res) =>{
 });
 
 app.post('/planta', async (req, res) => {
-    const {nome_popular, imagens, nomes_populares,
-        nome_cientifico, consumindo_por, partes_utilizadas,
-        formas_de_conservacao, formas_de_oferta_e_preparo,
-        composicao_de_proteina, composicao_carboidratos_totais,
-        curiosidades} = req.body
+    const { id,
+        nomePopular,
+        images,
+        nomesPopulares,
+        nomeCientifico,
+        nomeFamilias,
+        consumidorPor,
+        partesUtilizadas,
+        formasConservacao,
+        formasOfertaPreparo,
+        composiçãoProteica,
+        composicaoDeCarboidratos,
+        formasDePropagracao,
+        distribuicao,
+        composiçãoBromatologica,
+        consideracoesComposicao,
+        formasPropragacaoDetalhes,
+        produtividade,
+        neutralizacaoDeFatoresAnti,
+        desempenho,
+        curiosidades,
+        bibliografia, } = req.body
+
     const planta = {
-        nome_popular, imagens, nomes_populares,
-        nome_cientifico, consumindo_por, partes_utilizadas,
-        formas_de_conservacao, formas_de_oferta_e_preparo,
-        composicao_de_proteina, composicao_carboidratos_totais,
-        curiosidades
+        id,
+        nomePopular,
+        images,
+        nomesPopulares,
+        nomeCientifico,
+        nomeFamilias,
+        consumidorPor,
+        partesUtilizadas,
+        formasConservacao,
+        formasOfertaPreparo,
+        composiçãoProteica,
+        composicaoDeCarboidratos,
+        formasDePropagracao,
+        distribuicao,
+        composiçãoBromatologica,
+        consideracoesComposicao,
+        formasPropragacaoDetalhes,
+        produtividade,
+        neutralizacaoDeFatoresAnti,
+        desempenho,
+        curiosidades,
+        bibliografia,
     }
 
-    try{
+    try {
         await Planta.create(planta)
-        res.status(201).json({message: 'Planta adicionada com sucesso!'})
+        res.status(201).json({ message: 'Planta adicionada com sucesso!' })
 
-    } catch(error){
-        res.status(500).json({error: error})
+    } catch (error) {
+        res.status(500).json({ error: error })
 
     }
 });
--- models/Plants.js
+++ models/Plants.js
@@ -1,17 +1,38 @@
 const mongoose = require('mongoose')
 
 const Planta = mongoose.Schema({
-    nome_popular: String,
-    imagens: Array,
-    nomes_populares: [String],
-    nome_cientifico: String,
-    consumindo_por: [String],
-    partes_utilizadas: String,
-    formas_de_conservacao: String,
-    formas_de_oferta_e_preparo: String,
-    composicao_de_proteina: Number,
-    composicao_carboidratos_totais: Number,
-    curiosidades: String
+    id: Number,
+    nomePopular: String,
+    images: [
+        {
+            id: Number,
+            url: String
+        }
+    ],
+    nomesPopulares: String,
+    nomeCientifico: String,
+    nomeFamilias: String,
+    consumidorPor: String,
+    partesUtilizadas: String,
+    formasConservacao: String,
+    formasOfertaPreparo: String,
+    composiçãoProteica: Number,
+    composicaoDeCarboidratos: Number,
+    formasDePropagracao: String,
+    distribuicao: String,
+    composiçãoBromatologica: String,
+    consideracoesComposicao: String,
+    formasPropragacaoDetalhes: String,
+    produtividade: String,
+    neutralizacaoDeFatoresAnti: String,
+    desempenho: String,
+    curiosidades: String,
+    bibliografia:[
+        {
+            id: Number,
+            pesquisa: String
+        },
+    ]
 })
 
 module.exports = mongoose.model("Planta", Planta)
